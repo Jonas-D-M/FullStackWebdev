8134e3ec2325a40c658ed6966eb5f6b5d6d740c6
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-02-18 09:57:26 +0100

    oef02 & oef03

--- Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/afvalcontainer/script/app_antwerpen.js
+++ Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/afvalcontainer/script/app_antwerpen.js
@@ -1,36 +1,56 @@
 'use strict';
+let link = 'https://opendata.arcgis.com/datasets/413c00cfda8743fbb94ce7e7e67d67c7_49.geojson';
 
-const url = 'https://opendata.arcgis.com/datasets/413c00cfda8743fbb94ce7e7e67d67c7_49.geojson';
-
-/* STAP 2: Schrijf een loadData functie die - via FETCH - communiseert met de API */
-const loadData = function() {
-  fetch(url)
+const init = function() {
+  fetch(link)
     .then(function(response) {
       if (!response.ok) {
-        throw Error(`Probleem bij de fetch(). Status Code: ${response.status}`);
+        throw Error(`Looks like there was a problem. Status Code: ${response.status}`);
       } else {
-        console.info('Er is een response teruggekomen van de server');
         return response.json();
       }
     })
     .then(function(jsonObject) {
-      console.info('json object is aangemaakt');
-      console.info('verwerken data');
-      verwerkData(jsonObject);
+      verwerken(jsonObject);
     })
     .catch(function(error) {
       console.error(`fout bij verwerken json ${error}`);
     });
 };
-
-/* STAP 3: Als fetch gelukt is, wordt deze functie uitgevoerd om de data via INNERHTML te tonen */
-const verwerkData = function(json) {
-  console.log('Starting verwerkData');
-  console.log(json);
+const verwerken = function(json) {
+  let lijst = '';
+  for (let straat of json.features) {
+    lijst += `<div class="locatie">
+    <div class="adres">${straat.properties.STRAATNAAM}</div>
+    <div class="row">
+      <div>
+        GFT
+        <div class="aantal">${zeroToStreep(straat.properties.AANTAL_CNTR_GFT)}</div>
+      </div>
+      <div>
+        GLAS
+        <div class="aantal">${zeroToStreep(straat.properties.AANTAL_CNTR_GLAS)}</div>
+      </div>
+      <div>
+        PMD
+        <div class="aantal">${zeroToStreep(straat.properties.AANTAL_CNTR_PMD)}</div>
+      </div>
+      <div>
+        REST
+        <div class="aantal">${zeroToStreep(straat.properties.AANTAL_CNTR_REST)}</div>
+      </div>
+      <div>
+        PAPIER
+        <div class="aantal">${zeroToStreep(straat.properties.AANTAL_CNTR_PK)}</div>
+      </div>
+    </div>
+  </div>`;
+  }
+  document.querySelector('#containerLocaties').innerHTML = lijst;
 };
-
-/* STAP 1: Voeg DOMContentLoaded Event Listener toe  */
+const zeroToStreep = function(input) {
+  return input === 0 ? '---' : input;
+};
 document.addEventListener('DOMContentLoaded', function() {
-  console.info('DOM geladen');
-  loadData();
+  init();
 });
--- Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/weather/script/app_weather.js
+++ Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/weather/script/app_weather.js
@@ -1,8 +1,82 @@
 'use strict';
+let APIKey = '74b199735022be5d6c0444a30db06528';
+let uri = `http://api.openweathermap.org/data/2.5/forecast?q=kortrijk,BE&appid=${APIKey}&units=metric&lang=nl`;
+/*
+<div class="dag">
+  <div class="datum">Maandag</div>
+  <div class="afbeelding">
+    <img src="images/weather/wi-thunderstorm.svg" alt="Onweer" />
+  </div>
+  <div class="uitleg">
+    Onweer
+  </div>
+  <div class="min">10</div>
+  <div class="max">20</div>
+</div>
+*/
+const laadWeatherInfo = function() {
+  fetch(uri)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(`Looks like there was a problem. Status Code: ${response.status}`);
+      } else {
+        return response.json();
+      }
+    })
+    .then(function(jsonObject) {
+      verwerkWeer(jsonObject);
+    })
+    .catch(function(error) {
+      console.error(`fout bij verwerken json ${error}`);
+    });
+};
+const verwerkWeer = function(json) {
+  console.log(json);
+  document.querySelector('.js-city-placeholder').innerHTML = json.city.name;
+  let str = '';
+  let day = [];
+  let days = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag'];
 
-/* STAP 2 */
+  for (let i = 0; i < json.list.length; i += 8) {
+    let obj = json.list[i];
+    let date = new Date(obj.dt * 1000);
+    let daycount = date.getDay();
+    str += `<div class="c-forecast">
+            <div class="c-forecast__datum">${days[daycount]}</div>
+            <div class="c-forecast__sym">
+              <img src="images/weather/${weatherCodeToImage(obj.weather[0].id)}" alt="${obj.weather[0].description}" />
+            </div>
 
-/* STAP 3 */
+            <div class="c-forecast__uitleg">
+            ${obj.weather[0].description}
+            </div>
+            <div class="c-forecast__max">${Math.round(obj.main.temp_max)}&#176;C</div>
+            <div class="c-forecast__min">${Math.round(obj.main.temp_min)}&#176;C</div>
+           
+          </div>`;
+  }
+  document.querySelector('.js-weather-placeholder').innerHTML = str;
+};
+const weatherCodeToImage = function(code) {
+  let codeString = code.toString();
+  let firstDigit = codeString.substring(0, 1);
+  let specials = {
+    800: 'wi-day-sunny.svg',
+    801: 'wi-day-sunny-overcast.svg',
+    900: 'wi-thunderstorm.svg'
+  };
+  let normal = {
+    2: 'wi-thunderstorm.svg',
+    3: 'wi-sprinkle.svg',
+    5: 'wi-rain.svg',
+    6: 'wi-snow.svg',
+    7: 'wi-fog.svg',
+    8: 'wi-cloud.svg'
+  };
+  return specials[code] !== undefined ? specials[code] : normal[firstDigit] !== undefined ? normal[firstDigit] : 'wi-alien.svg';
+};
 
-/* STAP 1 */
-
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('DOMContent loaded');
+  laadWeatherInfo();
+});
