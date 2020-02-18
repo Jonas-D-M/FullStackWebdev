a7a6cf1be6b7eef90c15870d458a1c7d8dc6422b
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-02-18 09:19:27 +0100

    finnished oef01

--- Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/afvalcontainer/script/app_antwerpen.js
+++ Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/afvalcontainer/script/app_antwerpen.js
@@ -1,8 +1,36 @@
 'use strict';
 
+const url = 'https://opendata.arcgis.com/datasets/413c00cfda8743fbb94ce7e7e67d67c7_49.geojson';
+
 /* STAP 2: Schrijf een loadData functie die - via FETCH - communiseert met de API */
+const loadData = function() {
+  fetch(url)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(`Probleem bij de fetch(). Status Code: ${response.status}`);
+      } else {
+        console.info('Er is een response teruggekomen van de server');
+        return response.json();
+      }
+    })
+    .then(function(jsonObject) {
+      console.info('json object is aangemaakt');
+      console.info('verwerken data');
+      verwerkData(jsonObject);
+    })
+    .catch(function(error) {
+      console.error(`fout bij verwerken json ${error}`);
+    });
+};
 
 /* STAP 3: Als fetch gelukt is, wordt deze functie uitgevoerd om de data via INNERHTML te tonen */
+const verwerkData = function(json) {
+  console.log('Starting verwerkData');
+  console.log(json);
+};
 
 /* STAP 1: Voeg DOMContentLoaded Event Listener toe  */
-
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('DOM geladen');
+  loadData();
+});
--- Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/devtool/script/serie.js
+++ Week02/02 Events - JSON - Fetch - Startbestanden/startbestanden/devtool/script/serie.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const url = 'http://api.tvmaze.com/singlesearch/shows?q=Homeland&embed=episodes';
+
+const laadData = function() {
+  fetch(url)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(`Probleem bij de fetch(). Status Code: ${response.status}`);
+      } else {
+        console.info('Er is een response teruggekomen van de server');
+        return response.json();
+      }
+    })
+    .then(function(jsonObject) {
+      console.info('json object is aangemaakt');
+      console.info('verwerken data');
+      verwerkData(jsonObject);
+    })
+    .catch(function(error) {
+      console.error(`fout bij verwerken json ${error}`);
+    });
+};
+
+const verwerkData = function(json) {
+  let html = '';
+  console.log(json);
+
+  html += `<div>De naam van de serie is ${json.name}</div>
+  <div>Het uur waarop het wordt uitgezonden is ${json.schedule.time}</div>
+  <div>Volgende genres<ol>`;
+
+  for (let genre of json.genres) {
+    html += `<li>${genre}</li>`;
+  }
+  html += `</ol></div><div>Overzicht episodes<ul>`;
+
+  for (let episode of json._embedded.episodes) {
+    html += `<li>s${episode.season}-e${episode.number} - ${episode.name} </li>`;
+  }
+
+  html += `</ul></div>`;
+  document.querySelector('body').innerHTML = html;
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('DOM geladen');
+  laadData();
+});
