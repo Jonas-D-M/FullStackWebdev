43cf739f392103cc6387f0c8fe01594831fdba1f
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-02-25 09:47:12 +0100

    oefening 1 afgewerkt

--- FSWD_3_Labo_Startbestanden/1_radio_checkbox/script/script.js
+++ FSWD_3_Labo_Startbestanden/1_radio_checkbox/script/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const init = function() {
+  const range = document.querySelector('input[type=range]');
+  range.addEventListener('input', function() {
+    document.querySelector('.js-output').innerHTML = `${range.value}/10`;
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('DOM geladen');
+  init();
+});
