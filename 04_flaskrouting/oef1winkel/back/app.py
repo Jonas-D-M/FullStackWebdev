85ea38fe575d14c166f5732156949df3d39da536
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-03-03 11:26:37 +0100

    oef01 afgewerkt

--- 04_flaskrouting/oef1winkel/back/app.py
+++ 04_flaskrouting/oef1winkel/back/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -17,5 +17,29 @@ def hello_world():
     return 'ga naar de API url'
 
 
+@app.route('/api/v1/products')
+def products():
+    return dictStock, 200
+
+
+@app.route('/api/v1/payment', methods=['POST'])
+def payment():
+    try:
+        product_id = int(request.form["product"])
+        product_aantal = int(request.form["aantal"])
+
+        prijs = str(dictStock[product_id]['prijs'] * product_aantal)
+        if 0 < product_aantal < 1000 and product_id in dictStock.keys:
+            print(
+                f"{dictStock[product_id]['naam']}:\n\t-Besteld: {product_aantal} \n\t-Totaal prijs: {prijs}")
+            # status created, oké en iets gedaan
+            return jsonify(status="succes"), 201
+        else:
+            return jsonify(status="error"), 400
+    except Exception as e:
+        print(e)
+        return jsonify(status="error"), 500
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
