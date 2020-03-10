635e5f82c40ebdfe2255e9d0177c20b0ebd3e9bc
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-03-10 11:55:22 +0100

    klanten.js finished (SELECT query)

--- 05_FLASK_CRUD/frontend/script/klanten.js
+++ 05_FLASK_CRUD/frontend/script/klanten.js
@@ -1,17 +1,21 @@
-"use strict";
+'use strict';
 
-const toonKlanten = function (jsonObject) {
+const toonKlanten = function(jsonObject) {
   console.log(jsonObject);
   let htmlstring = `<table border="1" width="100%"><tr><th>FNaam</th><th>VNaam</th><th>Straat</th><th>Nummer</th><th>Postcode</th><th>Gemeente</th><th>verwijderen</th></tr>`;
-  htmlstring += `<tr><td>${klant.FNaam}</td><td>${klant.VNaam}</td><td>${klant.Straat}</td><td>${klant.Nummer}</td><td>${klant.Postcode}</td><td>${klant.Gemeente}</td></tr>`;
+  for (let klant of jsonObject.klanten) {
+    htmlstring += `<tr><td>${klant.FNaam}</td><td>${klant.VNaam}</td><td>${klant.Straat}</td><td>${klant.Nummer}</td><td>${klant.Postcode}</td><td>${klant.Gemeente}</td></tr>`;
+  }
   htmlstring += `</table>`;
+  document.querySelector('.js-klanten').innerHTML = htmlstring;
 };
 
-const init = function () {
-  console.info("Toont alle klanten");
+const init = function() {
+  console.info('Toont alle klanten');
+  handleData('http://localhost:5000/api/v1/klanten', toonKlanten);
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.info("DOM geladen");
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('DOM geladen');
   init();
 });
