344516e41b6e132bb7847ecca18bcffb607ce614
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-03-17 10:07:40 +0100

    added datarepository functions

--- 06_Labo_herhaling/bronmateriaal/backend/repositories/DataRepository.py
+++ 06_Labo_herhaling/bronmateriaal/backend/repositories/DataRepository.py
@@ -11,5 +11,37 @@ class DataRepository:
         return gegevens
 
     #########  Treinen  #########
+    @staticmethod
+    def read_treinen():
+        sql = "SELECT * FROM treinen"
+        return Database.get_rows(sql)
+
+    @staticmethod
+    def read_trein(treinid):
+        pass
+
+    @staticmethod
+    def create_trein():
+        pass
+
+    @staticmethod
+    def update_trein():
+        pass
+
+    @staticmethod
+    def delete_trein():
+        pass
+
+    @staticmethod
+    def update_trein_vertraging(idtrein, vertraging):
+        pass
 
     #########  Bestemmingen  #########
+    @staticmethod
+    def read_bestemmingen():
+        sql = "SELECT * FROM bestemmingen"
+        return Database.get_rows(sql)
+
+    @staticmethod
+    def read_treinen_met_bestemming(bestemmingid):
+        pass
