d7fe2f26ec0fd4ba971ea96e3ae5ef34a312356c
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-03-17 10:26:51 +0100

    added sql statements to datarepository functions

--- 06_Labo_herhaling/bronmateriaal/backend/repositories/DataRepository.py
+++ 06_Labo_herhaling/bronmateriaal/backend/repositories/DataRepository.py
@@ -18,23 +18,34 @@ class DataRepository:
 
     @staticmethod
     def read_trein(treinid):
-        pass
+        sql = "SELECT * FROM treinen WHERE idtrein = %s"
+        params = [treinid]
+        return Database.get_one_row(sql, params)
 
     @staticmethod
-    def create_trein():
-        pass
+    def create_trein(vertrek, bestemmingsID, spoor, vertraging, afgeschaft):
+        sql = "INSERT INTO treinen (vertrek, bestemmingsID, spoor) VALUES (%s, %s, %s)"
+        params = [vertrek, bestemmingsID, spoor, vertraging, afgeschaft]
+        return Database.execute_sql(sql, params)
 
     @staticmethod
-    def update_trein():
-        pass
+    def update_trein(vertrek, bestemmingsID, spoor, vertraging, afgeschaft, idtrein):
+        sql = "UPDATE treinen SET vertrek = %s, bestemmingsID = %s, spoor = %s, vertraging = %s, afgeschaft = %s WHERE idtrein = %s"
+        params = [vertrek, bestemmingsID, spoor,
+                  vertraging, afgeschaft, idtrein]
+        return Database.execute_sql(sql, params)
 
     @staticmethod
-    def delete_trein():
-        pass
+    def delete_trein(idtrein):
+        sql = "DELETE FROM treinen WHERE idtrein = %s"
+        params = [idtrein]
+        return Database.execute_sql(sql, params)
 
     @staticmethod
     def update_trein_vertraging(idtrein, vertraging):
-        pass
+        sql = "UPDATE treinen SET vertraging = %s WHERE idtrein= %s"
+        params = [vertraging, idtrein]
+        return Database.execute_sql(sql, params)
 
     #########  Bestemmingen  #########
     @staticmethod
@@ -44,4 +55,6 @@ class DataRepository:
 
     @staticmethod
     def read_treinen_met_bestemming(bestemmingid):
-        pass
+        sql = "SELECT * FROM treinen t INNER JOIN bestemmingen b ON t.bestemmingID = b.idbestemming WHERE idbestemming = %s"
+        params = [bestemmingid]
+        return Database.get_rows(sql, params)
