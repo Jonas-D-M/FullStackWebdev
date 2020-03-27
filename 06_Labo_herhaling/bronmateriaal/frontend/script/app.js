7bcc3b00ed512a138c5f88f08b3dfd940a7d0c18
Jonas-D-M <jonas.de.meyer@student.howest.be>
2020-03-27 14:50:25 +0100

    commit all

--- 06_Labo_herhaling/bronmateriaal/frontend/script/app.js
+++ 06_Labo_herhaling/bronmateriaal/frontend/script/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
+let destinationId;
+
 //#region ***  DOM references ***
-let html_bestemming, html_bestemmingen, html_destinationHolder, html_routeHolder, html_selectedCity, html_destinationSelect, html_adaptTrain;
+let html_destinationHolder, html_routeHolder, html_selectedCity, html_destinationSelect, html_adaptTrain;
 //#endregion
 
 //#region ***  Callback-Visualisation - show___ ***
@@ -18,9 +20,27 @@ const showDestination = function(jsonObject) {
   // start met het luisteren naar click event van de net aangemaakte menu items
   listenToClickDestination();
 };
+
+const showTrainsOnDestinations = function(jsonObject) {
+  if (jsonObject.length === 0) {
+    html_adaptTrain.html_routeHolder.innerHTML = 'Geen treinen.';
+    return;
+  }
+  listenToClickRemoveTrain();
+};
+
 //#endregion
 
 //#region ***  Callback-No Visualisation - callback___  ***
+const callbackAddTrain = function(data) {
+  console.log('ADD antw van server');
+  if (data.treinid > 0) {
+    console.log('ADD gelukt');
+  }
+};
+const callbackRemoveTrain = function(data) {
+  console.log('Remove antw van server');
+};
 //#endregion
 
 //#region ***  Data Access - get___ ***
@@ -28,6 +48,10 @@ const getBestemmingen = function() {
   console.info('Get bestemmingen');
   handleData('http://127.0.0.1:5000/api/v1/bestemmingen', showDestination);
 };
+const getTrainsOnDestinations = function(idDestination) {
+  handleData(`http://127.0.0.1:5000/api/v1/treinen/bestemming/${idDestination}`, showTrainsOnDestinations);
+};
+
 //#endregion
 
 //#region ***  Event Listeners - listenTo___ ***
@@ -35,8 +59,29 @@ const listenToClickDestination = function() {
   const buttons = document.querySelectorAll('.js-station');
   for (const btn of buttons) {
     btn.addEventListener('click', function() {
-      console.log(this);
-      console.log(this.innerHTML);
+      const id = this.getAttribute('data-destination-id');
+      destinationId = id;
+      getTrainsOnDestinations(id);
+    });
+  }
+};
+
+const listenToClickAddTrain = function() {
+  const button = document.querySelector('.js-add-train');
+  button.addEventListener('click', function() {
+    console.log('Toevoegen nieuwe trein');
+    const jsonObject = '';
+    console.log(jsonObject);
+    handleData('http://127.0.0.1:5000/api/v1/treinen', callbackAddTrain, null, 'POST', JSON.stringify(jsonObject));
+  });
+};
+const listenToClickRemoveTrain = function() {
+  const buttons = document.querySelectorAll('.c-traject__delete-symbol');
+  for (const b of buttons) {
+    b.addEventListener('click', function() {
+      const id = this.getAttribute('data-train-id');
+      console.log('verwijder ' + id);
+      handleData(`http://127.0.0.1:5000/api/v1/treinen/${id}`, callbackRemoveTrain, null, 'DELETE');
     });
   }
 };
@@ -45,8 +90,6 @@ const listenToClickDestination = function() {
 //#region ***  INIT / DOMContentLoaded  ***
 const init = function() {
   console.log('🚂', 'https://www.youtube.com/watch?v=8oVTXSntnA0');
-  // html_bestemmingen = document.querySelector('.js-destinations');
-  // html_bestemming = document.querySelector('.js-destination');
 
   html_destinationHolder = document.querySelector('.js-destinations');
   html_routeHolder = document.querySelector('.js-trajects');
